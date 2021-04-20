dcdeb992d75cc114a51a0b72585115791d9c6266
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-04-20 11:54:16 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,8 @@
 // 1. Print Exponential Values
 // Write a function that accepts two numbers and validate that they are numbers.
 // After that, the function should print y exponential values starting from x.
+// For example if we have function(3, 5) The function should print 3 9 27 81 243. Prints 5 exponential values of 3.
+// function(2, 8) The function prints 2 4 8 16 32 64 128 256. Prints 8 exponential values of 2.
 
 function exponentials(x,y){
 
@@ -15,20 +17,26 @@ function exponentials(x,y){
 }
 exponentials(7,8)
 
-// For example if we have function(3, 5) The function should print 3 9 27 81 243. Prints 5 exponential values of 3.
-// function(2, 8) The function prints 2 4 8 16 32 64 128 256. Prints 8 exponential values of 2.
+
+
+
+
 // 2. Fruits
 // Create a function named printFavoriteFruit. Declare a variable outside of the function and store your favorite fruit as a value. Reassign the variable within the function and print "My favorite fruit is: x".
+
+
 let fav_food 
 function printFavoriteFruit(){
     fav_food = "Mango"
 }
 printFavoriteFruit()
 console.log(fav_food)
+
+
 // 3. Multiply a Number by Itself
 // Create a function named exponent that takes two numbers as parameters. The second parameter defines how many times the first number should be multiplied by itself. Save the output in a variable named result.
 // Then, try to access the variable result outside of the exponent function. Is this possible? Why/Why not? Comment your answer in the index.js file.
-
+exponent(5,6)
 function exponent(a,b){
     if(!(isNaN(a)||isNaN(b))){
         let result =  a**b
